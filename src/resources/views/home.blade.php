cb0402e1197badf6e85fd49739fe1e0b54cf5b5f
codeW46253 <ezig.2005@gmail.com>
2026-01-16 15:22:55 +0800

    Created view: Home

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'StudyGram Home Page')
+
+@section('content')
+
+<br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="container text-center">
+                <h1 class="display-1 text-primary lead">Welcome to StudyGram</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="container text-break text-secondary">
+                <p>
+                    This web application was developed for students to
+                    share notes among each other.
+                </p>
+                <p>
+                    Users can use this web application for sharing notes.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
